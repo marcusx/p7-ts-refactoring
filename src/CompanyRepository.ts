4a1909275a7e243571cd4f4d514a6751141a00db
Marcus Exner <marcus.exner@gmail.com>
2019-08-12 14:02:24 +0200

    Update CompanyRepository so we can claim "VeryImportantClient" for passing the credit limit check.

--- src/CompanyRepository.ts
+++ src/CompanyRepository.ts
@@ -1,12 +1,20 @@
-﻿import { Company } from "./Company";
-import { Classification } from "./Classification";
+﻿import { Company } from './Company';
+import { Classification } from './Classification';
 
 export class CompanyRepository {
-    GetById(id): Company {
-        return {
-            Id: id,
-            Name: "test",
-            Classification: Classification.Bronze
-       }
+  GetById(id): Company {
+    if (id === 'bbv') {
+      return {
+        Id: id,
+        Name: 'VeryImportantClient',
+        Classification: Classification.Silver
+      };
+    } else {
+      return {
+        Id: id,
+        Name: 'test',
+        Classification: Classification.Bronze
+      };
     }
+  }
 }
