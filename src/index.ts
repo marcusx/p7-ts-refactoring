a5f041638170974576c8171d3d82ab3fbe744b66
Marcus Exner <marcus.exner@gmail.com>
2019-08-12 13:15:45 +0200

    Use iso date for testing.

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ console.log('Good morning');
 const customerService = new CustomerService();
 const addCustomer = customerService.AddCustomer(
   'Marcus',
-  'Exner',
+  '',
   'marcus.exner@bbv.eu',
-  '25.09.1977',
+  '1977-09-25',
   'bbv'
 );
 console.log(addCustomer);
